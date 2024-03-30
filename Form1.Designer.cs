5efb7d3e0082f32689fd09a198c76d06c92b6068
Rus Andreea <87426237+DEEA808@users.noreply.github.com>
2024-03-30 13:26:43 +0200

    Lab1DataBase
    – display all the records in the parent table;
    – display the child records for a specific (i.e., selected) parent record;
    – add / remove / update child records for a specific parent record.
    -next and previous buttons
    -text box for the price

---
+++ Form1.Designer.cs
@@ -0,0 +1,154 @@
+﻿namespace Lab2Forms
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridSupll = new System.Windows.Forms.DataGridView();
+            this.dataGridOrders = new System.Windows.Forms.DataGridView();
+            this.ConnectButton = new System.Windows.Forms.Button();
+            this.UpdateButton = new System.Windows.Forms.Button();
+            this.DeleteButton = new System.Windows.Forms.Button();
+            this.textBoxOrder = new System.Windows.Forms.TextBox();
+            this.NextButton = new System.Windows.Forms.Button();
+            this.PrevButton = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridSupll)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridOrders)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridSupll
+            // 
+            this.dataGridSupll.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridSupll.Location = new System.Drawing.Point(25, 135);
+            this.dataGridSupll.Name = "dataGridSupll";
+            this.dataGridSupll.RowHeadersWidth = 82;
+            this.dataGridSupll.RowTemplate.Height = 33;
+            this.dataGridSupll.Size = new System.Drawing.Size(554, 354);
+            this.dataGridSupll.TabIndex = 0;
+            // 
+            // dataGridOrders
+            // 
+            this.dataGridOrders.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridOrders.Location = new System.Drawing.Point(711, 135);
+            this.dataGridOrders.Name = "dataGridOrders";
+            this.dataGridOrders.RowHeadersWidth = 82;
+            this.dataGridOrders.RowTemplate.Height = 33;
+            this.dataGridOrders.Size = new System.Drawing.Size(532, 354);
+            this.dataGridOrders.TabIndex = 1;
+            // 
+            // ConnectButton
+            // 
+            this.ConnectButton.Location = new System.Drawing.Point(25, 577);
+            this.ConnectButton.Name = "ConnectButton";
+            this.ConnectButton.Size = new System.Drawing.Size(181, 62);
+            this.ConnectButton.TabIndex = 2;
+            this.ConnectButton.Text = "Connect";
+            this.ConnectButton.UseVisualStyleBackColor = true;
+            this.ConnectButton.Click += new System.EventHandler(this.ConnectButton_Click_1);
+            // 
+            // UpdateButton
+            // 
+            this.UpdateButton.Location = new System.Drawing.Point(550, 551);
+            this.UpdateButton.Name = "UpdateButton";
+            this.UpdateButton.Size = new System.Drawing.Size(187, 60);
+            this.UpdateButton.TabIndex = 3;
+            this.UpdateButton.Text = "Update";
+            this.UpdateButton.UseVisualStyleBackColor = true;
+            this.UpdateButton.Click += new System.EventHandler(this.UpdateButton_Click);
+            // 
+            // DeleteButton
+            // 
+            this.DeleteButton.Location = new System.Drawing.Point(541, 655);
+            this.DeleteButton.Name = "DeleteButton";
+            this.DeleteButton.Size = new System.Drawing.Size(206, 60);
+            this.DeleteButton.TabIndex = 4;
+            this.DeleteButton.Text = "Delete";
+            this.DeleteButton.UseVisualStyleBackColor = true;
+            this.DeleteButton.Click += new System.EventHandler(this.DeleteButton_Click);
+            // 
+            // textBoxOrder
+            // 
+            this.textBoxOrder.Location = new System.Drawing.Point(1114, 509);
+            this.textBoxOrder.Name = "textBoxOrder";
+            this.textBoxOrder.Size = new System.Drawing.Size(129, 31);
+            this.textBoxOrder.TabIndex = 5;
+            // 
+            // NextButton
+            // 
+            this.NextButton.Location = new System.Drawing.Point(1072, 600);
+            this.NextButton.Name = "NextButton";
+            this.NextButton.Size = new System.Drawing.Size(141, 51);
+            this.NextButton.TabIndex = 6;
+            this.NextButton.Text = "Next";
+            this.NextButton.UseVisualStyleBackColor = true;
+            this.NextButton.Click += new System.EventHandler(this.NextButton_Click);
+            // 
+            // PrevButton
+            // 
+            this.PrevButton.Location = new System.Drawing.Point(858, 600);
+            this.PrevButton.Name = "PrevButton";
+            this.PrevButton.Size = new System.Drawing.Size(141, 52);
+            this.PrevButton.TabIndex = 7;
+            this.PrevButton.Text = "Prev";
+            this.PrevButton.UseVisualStyleBackColor = true;
+            this.PrevButton.Click += new System.EventHandler(this.PrevButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1274, 780);
+            this.Controls.Add(this.PrevButton);
+            this.Controls.Add(this.NextButton);
+            this.Controls.Add(this.textBoxOrder);
+            this.Controls.Add(this.DeleteButton);
+            this.Controls.Add(this.UpdateButton);
+            this.Controls.Add(this.ConnectButton);
+            this.Controls.Add(this.dataGridOrders);
+            this.Controls.Add(this.dataGridSupll);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridSupll)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridOrders)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridSupll;
+        private System.Windows.Forms.DataGridView dataGridOrders;
+        private System.Windows.Forms.Button ConnectButton;
+        private System.Windows.Forms.Button UpdateButton;
+        private System.Windows.Forms.Button DeleteButton;
+        private System.Windows.Forms.TextBox textBoxOrder;
+        private System.Windows.Forms.Button NextButton;
+        private System.Windows.Forms.Button PrevButton;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Lab2Forms
+{
+    public partial class Form1 : Form
+    {
+        SqlConnection conn = new SqlConnection("Server=deea888;Database=GuitarStore;Integrated Security=true;");
+        SqlDataAdapter adapterSuppliers, adapterOrders;
+        DataSet ds = new DataSet();
+        BindingSource bsSuppliers = new BindingSource();
+        BindingSource bsOrders = new BindingSource();
+        DataRelation dataRelation;
+        SqlCommandBuilder command;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void UpdateButton_Click(object sender, EventArgs e)
+        {
+            adapterOrders.Update(ds, "ORDERS");
+
+        }
+
+        private void DeleteButton_Click(object sender, EventArgs e)
+        {
+            if (dataGridOrders.SelectedRows.Count > 0)
+            {
+                foreach (DataGridViewRow row in dataGridOrders.SelectedRows)
+                {
+                    dataGridOrders.Rows.Remove(row);
+                }
+                adapterOrders.Update(ds, "ORDERS");
+            }
+        }
+
+        private void NextButton_Click(object sender, EventArgs e)
+        {
+            bsOrders.MoveNext();
+            dataGridOrders.ClearSelection();
+            dataGridOrders.Rows[bsOrders.Position].Selected = true;
+        }
+
+        private void PrevButton_Click(object sender, EventArgs e)
+        {
+            bsOrders.MovePrevious();
+            dataGridOrders.ClearSelection();
+            dataGridOrders.Rows[bsOrders.Position].Selected = true;
+        }
+
+        private void ConnectButton_Click_1(object sender, EventArgs e)
+        {
+            adapterSuppliers = new SqlDataAdapter("SELECT * FROM SUPPLIERS", conn);
+            adapterOrders = new SqlDataAdapter("SELECT * FROM ORDERS", conn);
+            command = new SqlCommandBuilder(adapterOrders);
+
+            adapterSuppliers.Fill(ds, "SUPPLIERS");
+            adapterOrders.Fill(ds, "ORDERS");
+            dataRelation = new DataRelation("FK_Suppliers_Orders",
+                ds.Tables["SUPPLIERS"].Columns["SupplierID"],
+                ds.Tables["ORDERS"].Columns["supplier_id"]);
+            ds.Relations.Add(dataRelation);
+
+            bsSuppliers.DataSource = ds;
+            bsSuppliers.DataMember = "SUPPLIERS";
+            bsOrders.DataSource = bsSuppliers;
+            bsOrders.DataMember = "FK_Suppliers_Orders";
+            this.dataGridSupll.DataSource = bsSuppliers;
+            this.dataGridSupll.ReadOnly = true;
+
+            this.dataGridOrders.DataSource = bsOrders;
+            this.textBoxOrder.DataBindings.Add("Text", bsOrders, "TotalPrice");
+            this.textBoxOrder.BackColor= Color.MediumPurple;
+
+
+        }
+    }
+}
